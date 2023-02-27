df40f061b70575119bc713432c28a76ebe9e7843
mounitbhandari <tataibhandari@gmail.com>
2023-02-27 20:58:09 +0530

    updated

--- src/app/pages/manual-result/manual-result.component.ts
+++ src/app/pages/manual-result/manual-result.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormGroup, Validators, FormControl} from "@angular/forms";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-manual-result',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./manual-result.component.scss']
 })
 export class ManualResultComponent {
+
+  manualResultActive = false;
+
+
+  highSecurityPassword: FormGroup;
+
+
+  constructor() {
+    this.highSecurityPassword = new FormGroup({
+      password: new FormControl(null, [Validators.required]),
+    });
+  }
+
+
+  validatePassword(){
+    if(this.highSecurityPassword.value.password == '1234@admin'){
+      this.manualResultActive = true;
+    }else{
+      Swal.fire({
+        position: 'top-end',
+        icon: 'info',
+        title: 'Invalid Password',
+        showConfirmButton: false,
+        timer: 2000
+      });
+      this.highSecurityPassword.reset();
+    }
+
+  }
 
 }
--- src/app/pages/manual-result/manual-result.module.ts
+++ src/app/pages/manual-result/manual-result.module.ts
@@ -3,6 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ManualResultRoutingModule } from './manual-result-routing.module';
 import { ManualResultComponent } from './manual-result.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {DialogModule} from "@angular/cdk/dialog";
+import {MatInputModule} from "@angular/material/input";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatIconModule} from "@angular/material/icon";
+
+
 
 
 @NgModule({
@@ -11,7 +21,17 @@ import { ManualResultComponent } from './manual-result.component';
   ],
   imports: [
     CommonModule,
-    ManualResultRoutingModule
+    ManualResultRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogModule,
+    DialogModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule
   ]
 })
 export class ManualResultModule { }
